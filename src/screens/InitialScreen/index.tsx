bdb9a04df9d56e1164bf05cf5d4179bbe43d33c9
Vaibhav Kushwaha <vaibhavvast6529@gmail.com>
2022-09-09 10:42:14 +0530

    navigation to signup and signin

--- src/screens/InitialScreen/index.tsx
+++ src/screens/InitialScreen/index.tsx
@@ -1,7 +1,9 @@
 import React, {FC, useMemo} from 'react';
-import {Text, View} from 'react-native';
+import {ImageBackground, Pressable, Image, Text, View} from 'react-native';
 import {NavigationProp, ParamListBase} from '@react-navigation/native';
 import createStyles from './style';
+import {INITIAL, LOGO} from '../../assets';
+import Button from '../../components/Button';
 
 interface IProps {
   navigation: NavigationProp<ParamListBase>;
@@ -12,6 +14,22 @@ const InitialScreen: FC<IProps> = ({navigation}) => {
   return (
     <View style={styles.body}>
       <Text style={styles.text}>InitialScreen</Text>
+      <ImageBackground source={INITIAL} style={styles.background}>
+        <Image source={LOGO} style={styles.logo} />
+        <Text style={styles.text}>
+          It hurts, but one day it will be just warm up
+        </Text>
+        <View style={styles.bottom}>
+          <Button
+            title="Get Started"
+            disabled={false}
+            onPress={() => navigation.navigate('SignupScreen')}
+          />
+          <Pressable onPress={() => navigation.navigate('SigninScreen')}>
+            <Text style={styles.loginText}>Or login</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
--- src/screens/InitialScreen/style.ts
+++ src/screens/InitialScreen/style.ts
@@ -12,7 +12,33 @@ const createStyles = () =>
         text: {
             fontSize: RFValue(25),
             fontWeight: '300',
-            color: Colors.BLACK,
+            color: Colors.LIGHT_GREY,
+            textAlign: 'center',
+            marginHorizontal: RFValue(50),
+        },
+        background: {
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+        },
+        logo: {
+            width: RFValue(130),
+            height: RFValue(130),
+            marginTop: RFValue(50),
+            marginBottom: RFValue(20),
+        },
+        bottom: {
+            position: 'absolute',
+            bottom: 0,
+            width: '100%'
+        },
+        loginText: {
+            textAlign: 'center',
+            fontSize: RFValue(20),
+            fontWeight: '500',
+            marginTop: RFValue(20),
+            color: Colors.LIGHT_GREY,
+            marginBottom: RFValue(50),
         }
     })
 
